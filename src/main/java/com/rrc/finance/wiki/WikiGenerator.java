d796d2449b07ed126c484a0dc0470560f28e0e63
doujinlong <doujinlong@renrenche.com>
2017-12-26 19:33:04 +0800

    修复无postJson的bug

--- src/main/java/com/rrc/finance/wiki/WikiGenerator.java
+++ src/main/java/com/rrc/finance/wiki/WikiGenerator.java
@@ -51,6 +51,7 @@ public abstract class WikiGenerator {
 		}
 		WikiParam result = new WikiParam();
 		result.setMethod(method.toString());
+		result.setPostJson(json);
 		final int index = url.indexOf('?');
 		result.setUrl(index>0?url.substring(0,index):url);
 		if (method.equals(HttpMethod.GET)) {
